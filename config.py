d6ecf22cf538136dd8d4c36c012947c2e574c0ec
skcicd <skcicd@gmail.com>
2025-01-29 09:10:23 +0200

    move app.db to instance folder and update config.py

--- config.py
+++ config.py
@@ -5,10 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')  # Use a strong key in production
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"  # Path to SQLite database
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(INSTANCE_DIR, 'app.db')}"  # Path to SQLite database
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable track modifications for performance
     OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
     DEBUG = False
